7f0158220b1e8d66ef174796dd30d41716162eca
Juliano de Barcellos <juliano.barcellos@gmail.com>
2018-11-22 22:29:21 -0200

    Criado um componente proprio para listagem das imagens, separando melhor o codigo

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from "@angular/core";
-import { PhotoComponent } from "./photo/photo.component";
-import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoComponent } from './photo/photo.component';
 
 @NgModule ({
-    exports: [ 
-        PhotoComponent,
-    ],
     declarations: [
         PhotoComponent,
+        PhotoListComponent,
     ],
     imports:[
         HttpClientModule,
