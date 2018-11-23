eb8e0a686aca8c8e6837e5e570bdffe5c5ef39db
Juliano de Barcellos <juliano.barcellos@gmail.com>
2018-11-22 23:20:59 -0200

    Criado modulo de rotas e photo-form.component para testar duas rotas diferentes

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoComponent } from './photo/photo.component';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
 
 @NgModule ({
     declarations: [
         PhotoComponent,
         PhotoListComponent,
+        PhotoFormComponent,
     ],
     imports:[
         HttpClientModule,
