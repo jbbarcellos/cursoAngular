dfe7385cd487a2c01fdfd8f134b7632e3914a29c
Juliano de Barcellos <juliano.barcellos@gmail.com>
2018-11-27 22:01:28 -0200

    Lidando com rotas inexistentes, criando modulo errors e componente not-foud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
 import { AppRoutingModule } from './app.routing.module';
+import { ErrorsModule } from './errors/errors.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { AppRoutingModule } from './app.routing.module';
     BrowserModule,
     PhotosModule,
     AppRoutingModule,
+    ErrorsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
